01da34ec23a8665819a482d6c23ce310541ff5f4
Frederick Javalera <Frederick.Javalera+regions@regions.com>
2025-04-14 17:12:35 -0600

    Adding validation to login component.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,22 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-    selector: 'login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css'],
-    standalone: false
+  selector: "login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
+  standalone: false,
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit() {}
 
-  constructor() {
-
-
+  login(loginForm: NgForm, submit: SubmitEvent) {
+    console.log(loginForm.value, loginForm.valid, submit);
   }
-
-  ngOnInit() {
-
-  }
-
 }
